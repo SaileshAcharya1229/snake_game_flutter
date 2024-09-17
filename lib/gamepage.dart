f12d3da94c4159b579924e18345c965a304e5eab
SaileshAcharya1229 <saileshacharya1229@gmail.com>
2024-09-17 21:59:17 +0545

    Adding design and control pannel

--- lib/gamepage.dart
+++ lib/gamepage.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class SnakeGamePage extends StatefulWidget {
@@ -7,9 +9,39 @@ class SnakeGamePage extends StatefulWidget {
   State<SnakeGamePage> createState() => _SnakeGamePageState();
 }
 
+enum Direction { up, down, left, right }
+
 class _SnakeGamePageState extends State<SnakeGamePage> {
   int row = 20, column = 20;
+  List<int> borderList = [];
+  List<int> snakePosition = [];
+  int snakeHead = 0;
+  int score = 0;
+  late Direction direction;
   @override
+  void initState() {
+    startGame();
+    super.initState();
+  }
+
+  void startGame() {
+    makeBorder();
+    direction = Direction.right;
+    snakePosition = [45, 44, 43];
+    snakeHead = snakePosition.first;
+    Timer.periodic(const Duration(milliseconds: 300), (timer) {
+      updateSnake();
+    });
+  }
+
+  void updateSnake() {
+    setState(() {
+      snakePosition.insert(0, snakeHead + 1);
+    });
+    snakePosition.removeLast();
+    snakeHead = snakePosition.first;
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
@@ -29,7 +61,8 @@ class _SnakeGamePageState extends State<SnakeGamePage> {
         return Container(
           margin: EdgeInsets.all(1),
           decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8), color: Colors.red),
+              borderRadius: BorderRadius.circular(8),
+              color: fillBoxColor(index)),
         );
       },
       itemCount: row * column,
@@ -37,10 +70,76 @@ class _SnakeGamePageState extends State<SnakeGamePage> {
   }
 
   Widget _buildGameControl() {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.all(20),
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("score:$score"),
+          IconButton(
+            onPressed: () {
+              if (direction != Direction.down) direction = Direction.up;
+            },
+            icon: const Icon(Icons.arrow_circle_up),
+            iconSize: 100,
+          ),
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {
+                  if (direction != Direction.right) direction = Direction.left;
+                },
+                icon: const Icon(Icons.arrow_circle_left_outlined),
+                iconSize: 100,
+              ),
+              const SizedBox(
+                width: 100,
+              ),
+              IconButton(
+                onPressed: () {
+                  if (direction != Direction.left) direction = Direction.right;
+                },
+                icon: const Icon(Icons.arrow_circle_right_outlined),
+                iconSize: 100,
+              ),
+            ],
+          ),
+          IconButton(
+            onPressed: () {
+              if (direction != Direction.up) direction = Direction.down;
+            },
+            icon: const Icon(Icons.arrow_circle_down),
+            iconSize: 100,
+          ),
+        ],
+      ),
+    );
   }
 
-  Color fillBoxColor() {
-    return Colors.red;
+  Color fillBoxColor(int index) {
+    if (borderList.contains(index))
+      return Colors.yellow;
+    else {
+      if (snakePosition.contains(index)) {
+        return Colors.green;
+      }
+    }
+    return Colors.grey.withOpacity(0.05);
+  }
+
+  makeBorder() {
+    for (int i = 0; i < column; i++) {
+      if (!borderList.contains(i)) borderList.add(i);
+    }
+    for (int i = 0; i < row * column; i = i + column) {
+      if (!borderList.contains(i)) borderList.add(i);
+    }
+    for (int i = column - 1; i < row * column; i = i + column) {
+      if (!borderList.contains(i)) borderList.add(i);
+    }
+    for (int i = (column * row) - column; i < row * column; i = i + 1) {
+      if (!borderList.contains(i)) borderList.add(i);
+    }
   }
 }
